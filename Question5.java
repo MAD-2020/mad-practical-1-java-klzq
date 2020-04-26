b30a98d74c5a33b5250b98a82ec679c31795df9e
klzq <64146095+klzq@users.noreply.github.com>
2020-04-26 17:50:36 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,30 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    int occur = 0;
+    int mode = 0;
+    ArrayList<Integer> data = new ArrayList<>();
     
+    while (occur == 0 || data.size() < 5)
+    {
+      System.out.prinnt("Enter a number: ");
+      int number = in.nextInt();
+      data.add(number)
+        for (int a = 0; a < data.size(); a++)
+        {
+          int occ = 0;
+          for (int b = 0; b < data.size(); a++)
+          {
+            if (data.get(a) == data.get(b))
+            {
+              occ +== 1;
+              if ( occ - 1 > occur)
+              {
+                occur = occ;
+                mode = data.get(a);
+              }
+            }
+          }
+        }
   }
 }
