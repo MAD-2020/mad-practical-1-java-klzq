6fdfe74fd2577207a027812166c299e5db65581a
klzq <64146095+klzq@users.noreply.github.com>
2020-04-26 12:55:41 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,10 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter an integer: ");
+    int number = in.nextInt();
+    System.out.printIn(num * num);
+      
     
   }
 }
